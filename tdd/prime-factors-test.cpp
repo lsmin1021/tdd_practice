65fccdf65f2deb68d0ad6139652749196b7ede03
smax <dltmdals826@gmail.com>
2025-05-02 13:57:55 +0900

    [feature] 44p

--- tdd/prime-factors-test.cpp
+++ tdd/prime-factors-test.cpp
@@ -22,3 +22,7 @@ TEST_F(PrimeFixture, Of3) {
 	vector<int> expected = { 3 };
 	EXPECT_EQ(expected, prime_factor.of(3));
 }
+TEST_F(PrimeFixture, Of4) {
+	vector<int> expected = { 2, 2 };
+	EXPECT_EQ(expected, prime_factor.of(4));
+}
