305e8393ba56fb40510b8c29adb50b4ed10bc683
smax <dltmdals826@gmail.com>
2025-05-02 13:54:21 +0900

    [feature] 37p

--- tdd/prime-factors-test.cpp
+++ tdd/prime-factors-test.cpp
@@ -3,8 +3,14 @@
 #include "prime-factors.cpp"
 
 
-TEST(PrimeFactors, PrimeTest) {
+TEST(PrimeFactors, Of1) {
 	PrimeFactors prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
--- tdd/prime-factors.cpp
+++ tdd/prime-factors.cpp
@@ -3,7 +3,12 @@
 using std::vector;
 class PrimeFactors {
 public:
-	vector<int> of(int i) {
-		return {};
+	vector<int> of(int number) {
+		vector<int> result = {};
+
+		if (number == 2) {
+			result.push_back(2);
+		}
+		return result;
 	}
 };
